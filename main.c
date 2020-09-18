635d587ab96b6d6a2361070d13d5c60928c3f99c
Jocelyn Fafard <jqf5530@psu.edu>
2020-09-18 23:03:40 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+
+int digit_sum(int n) 
+{ 
+    if (n == 0) 
+       return 0; 
+    else
+      return (n % 10 + digit_sum(n / 10)); 
+} 
+
+int main(void) 
+{ 
+    int n;
+    char *num = readline("Enter an int: ");
+    n = atoi(num);
+    int sum = digit_sum(n); 
+    printf("sum of digits in %d is %d.\n", n, sum); 
+    return 0; 
+}
